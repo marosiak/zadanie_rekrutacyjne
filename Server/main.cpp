dae60463d1c46f636547caa8d0cfcc5bec44e957
orzel1244 <orzel1244@gmail.com>
2018-04-07 23:48:13 +0200

    Oczyszczenie śmieciowych debugów

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,16 +1,11 @@
 #include <QCoreApplication>
 #include <Server/server.h>
 
-// test only
-#include <Xml/ResponseGenerator/responsegenerator.h>
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
     Server srv;
     srv.startServer(2222);
-
-    ResponseGenerator rg(LoginFailed);
-    qDebug() << rg.getOutput();
 
 
     return a.exec();
